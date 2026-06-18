7bd484dd09f3faa2377b96f983bcf16c440db525
Fabiano Bonin <fabiano.bonin@personalsoft.com.br>
2026-06-18 12:59:49 -0300

    feat: add daily analytical report component with meta and styles
    - Implemented the daily analytical report component in `index.jsx` with data filtering, grouping, and formatting.
    - Created a `meta.json` file to define report properties and parameters.
    - Added CSS and SCSS styles for the report layout and footer styling.
    - Established a template configuration in `template.json` for report assets and data source.
    - Updated global styles to enhance report presentation and layout consistency.

---
+++ playground/custom/concept/catalog/product/report/productPackingLabel/index.jsx
@@ -0,0 +1,71 @@
+import * as utils from "./utils.jsx";
+
+export default function ({ data = [], t }) {
+
+  data = data.reduce((red, e) => {
+    if (!red[e.product.id]) {
+      red[e.product.id] = [];
+    }
+    red[e.product.id].push(e);
+    return red;
+  }, {});
+
+  return (
+    <div className="report-wrapper">
+      {Object.values(data).map((arr, index) => {
+        const product = arr[0].product;
+        return (
+          <div className="report-container">
+            <main className="flex v gap">
+              <div className="flex v">
+                <div className="content flex h flex-1">
+                  <div className="flex v" style={{ flex: "3" }}>
+                    <dl className="flex-1">
+                      <dt>Artigo</dt>
+                      <dd>{`${product.code} - ${product.description}`}</dd>
+                    </dl>
+                    <dl className="flex-1">
+                      <dt>Composição</dt>
+                      <dd>{product.properties.textileComposition}</dd>
+                    </dl>
+                    <div className="flex h flex-1">
+                      <dl style={{ flex: "1 0 0" }}>
+                        <dt>Largura</dt>
+                        <dd>{product.properties?.textileWidth ? `${utils.formatNumber(product.properties?.textileWidth)} m` : "\u00A0"}</dd>
+                      </dl>
+                      <dl style={{ flex: "1 0 0" }}>
+                        <dt>Gramatura</dt>
+                        <dd>{product.properties?.textileGramWeight ? `${utils.formatNumber(product.properties?.textileGramWeight)} g/m²` : "\u00A0"}</dd>
+                      </dl>
+                      <dl style={{ flex: "1 0 0" }}>
+                        <dt>Rendimento</dt>
+                        <dd>{product.properties?.textileYield ? `${utils.formatNumber(product.properties?.textileYield)} m/kg` : "\u00A0"}</dd>
+                      </dl>
+                    </div>
+                  </div>
+                  <div className="flex v" style={{ flex: "1" }}>
+                    <dl style={{ gridArea: "H" }}>
+                      <dd><img src={`https://barcode.zensoft.com.br?bcid=qrcode&text=${product.barcode ?? product.code}`} /></dd>
+                    </dl>
+                  </div>
+                </div>
+              </div>
+              <div style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(3, 1fr)",
+                gap: "var(--gap)",
+              }}>
+                {arr.map((item) => (
+                  <dl key={item.id} className="flex v align-center justify-center padding no-break">
+                    <div>{item.code}</div>
+                    <div style={{ textAlign: "center" }}>{item.variant?.description}</div>
+                  </dl>
+                ))}
+              </div>
+            </main>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
---
+++ playground/custom/concept/commercial/sale/report/fechamentoDiarioAnalitico/index.jsx
@@ -0,0 +1,155 @@
+import * as utils from "./utils.jsx";
+import { Badge, Column, getVisibleColumns, GroupSections, Table } from "./utils.jsx";
+
+export default function ({ data = [], meta = {}, t }) {
+  const { report = {} } = meta;
+
+  const columns = [
+    { id: "group",
+      header: null,
+      width: "15ch",
+      cell: ({ value }) => <Badge>{value}</Badge>,
+    },
+    { id: "topic",
+      header: "Origem",
+      width: "15ch",
+      cell: ({ value }) => <Badge>{value}</Badge>,
+    },
+    { id: "quantity_today",
+      header: "m",
+      className: "number",
+      width: "15ch",
+      cell: ({ value }) => utils.formatNumber(value, { digits: 2 }),
+      footerValue: ({ data }) => data.reduce((acc, row) => acc + row.quantity_today, 0),
+      footer: ({ value }) => utils.formatNumber(value, { digits: 2 }),
+    },
+    {
+      "id": "unitValue",
+      "header": "R$/m",
+      "className": "number",
+      "width": "15ch",
+      "cellValue": ({ row }) => row.totalValue_today / row.quantity_today,
+      "cell": ({ value }) => utils.formatCurrency(value, { digits: 2 }),
+    },
+    { id: "totalValue_today",
+      header: "R$",
+      className: "number",
+      width: "15ch",
+      cell: ({ value }) => utils.formatCurrency(value, { digits: 2 }),
+      footerValue: ({ data }) => data.reduce((acc, row) => acc + row.totalValue_today, 0),
+      footer: ({ value }) => utils.formatCurrency(value, { digits: 2 }),
+    },
+    { id: "person_id",
+      header: "Cliente, Id",
+      width: "10ch",
+    },
+    { id: "person_name",
+      header: "Cliente",
+      width: "20ch",
+    },
+    { id: "manager_id",
+      header: "Supervisor, Id",
+      width: "10ch",
+    },
+    { id: "manager_code",
+      header: "Supervisor",
+      width: "20ch",
+      cell: ({ value }) => <Badge>{value}</Badge>,
+    },
+    { id: "personSalesperson_id",
+      header: "Vendedor, Id",
+      width: "10ch",
+    },
+    { id: "personSalesperson_name",
+      header: "Vendedor",
+      width: "20ch",
+      cell: ({ value }) => <Badge>{value}</Badge>,
+    },
+    { id: "product_id",
+      header: "Produto, Id",
+      width: "10ch",
+    },
+    { id: "product_code",
+      header: "Produto, Código",
+      width: "10ch",
+    },
+    { id: "product_description",
+      header: "Produto, Nome",
+      width: "25ch",
+    },
+  ];
+
+  data = data.filter(row => row.quantity_today !== 0);
+
+  data = utils.sort(data, [
+    { "columnId": "index" }, 
+    { "columnId": "index2" },
+    { "columnId": "manager_code" },
+    { "columnId": "personSalesperson_name" },
+    { "columnId": "person_name" },
+    { "columnId": "product_description" },
+  ]);
+  
+  const groupConfig = [
+    { "columnId": "topic" },
+    { "columnId": "manager_code" },
+    { "columnId": "personSalesperson_name" },
+    { "columnId": "person_name" },
+  ];
+  data = utils.group(data, groupConfig);
+  
+  const visibleColumns = getVisibleColumns({
+    availableColumns: columns.map(column => column.id),
+    overrideColumns: [
+      // "topic",
+      // "manager_code",
+      // "personSalesperson_name",
+      // "person_name",
+      "product_code",
+      "product_description",
+      "quantity_today",
+      "unitValue",
+      "totalValue_today",
+    ],
+    standardColumns:  [
+      "person_id",
+      "quantity_today",
+      "totalValue_today",
+    ],
+    addColumns: report.properties?.showColumns?.split(","),
+    removeColumns: report.properties?.hideColumns?.split(","),
+  });
+
+  return (
+    <div className="report-wrapper">
+      <div className="report-container a4 landscape">
+        <header>
+          <h1>Fechamento diário analítico</h1>
+          <section className="parameters">
+            <dl>
+              <dt>Data</dt>
+              <dd>{utils.formatDate(new Date())}</dd>
+            </dl>
+          </section>
+        </header>
+        <main>
+          <GroupSections 
+            columns={columns}
+            data={data} 
+            groups={groupConfig}>
+            {(group2Data) => (
+              <div className="content">
+                <Table data={group2Data}
+                  visibleColumns={visibleColumns}>
+                  {columns.map((column, index) => (
+                    <Column key={index} {...column} />
+                  ))}
+                </Table>
+              </div>
+            )}
+          </GroupSections>
+        </main>
+      </div>
+    </div>
+  );
+}
